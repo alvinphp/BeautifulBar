f06650a819c306b2bdc4dbd6605d7d50c31327cb
Alvin Gil Saldaña <alvingil86@gmail.com>
2025-11-29 17:50:38 -0500

    Update BeautifulBar.Class.php

--- BeautifulBar.Class.php
+++ BeautifulBar.Class.php
@@ -1,40 +1,40 @@
-<?php 
- /** 
-  * Clase BeautifulBar
-  * clase para cambiar el color de la barra de busqueda 
-  * en dispositivos moviles
-  */
- class BeautifulBar
- {  
-           // funcion para validar el codigo del color 
-    public static function ValidateColor($color){
+    <?php 
+     /** 
+      * Clase BeautifulBar
+      * clase para cambiar el color de la barra de busqueda 
+      * en dispositivos moviles
+      */
+     class BeautifulBar
+     {  
+               // funcion para validar el codigo del color 
+        public static function ValidateColor($color){
 
-      return preg_match('/^#[0-9A-Fa-f]{6}$/', $color);
-  }
+          return preg_match('/^#[0-9A-Fa-f]{6}$/', $color);
+      }
 
- 	// agregar color a la barra de estado
-  public static function AddColorBar($color = '#000000'){
+     	// agregar color a la barra de estado
+      public static function AddColorBar($color = '#000000'){
 
-      if(!self::ValidateColor($color)){
+          if(!self::ValidateColor($color)){
 
-       throw new Exception("The color '$color' is not valid. It must be a hexadecimal code.");
-   }
+           throw new Exception("The color '$color' is not valid. It must be a hexadecimal code.");
+       }
 
-      // add tag
+          // add tag
 
-   echo '<meta name="theme-color" content="' . $color . '">';
+       echo '<meta name="theme-color" content="' . $color . '">';
 
-}
- 	// funcion para manejar excepciones
-public static function AddColorBarBrowser($color = '#000000'){
-
-    try {
-    	self::AddColorBar($color);
-
-    } catch (Exception $e) {
-
-        echo 'Oops! Something went wrong:' . $e->getMessage();
     }
-}
-}
+     	// funcion para manejar excepciones
+    public static function AddColorBarBrowser($color = '#000000'){
+
+        try {
+        	self::AddColorBar($color);
+
+        } catch (Exception $e) {
+
+            echo 'Oops! Something went wrong:' . $e->getMessage();
+        }
+    }
+    }
 
